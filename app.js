b9491723b1dfb482934e8a95a80735cb3a0cad81
Stanley Louis <stanlouis@msn.com>
2019-06-26 19:45:02 -0400

    Add GET request

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({ name: 'Stanley Louis', app: 'Natours' });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours
+    }
+  });
 });
 
 const PORT = 3000;
