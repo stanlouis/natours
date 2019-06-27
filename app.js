b2199e03d63e4f2a0a58eb426b0074d641a84422
Stanley Louis <stanlouis@msn.com>
2019-06-26 20:22:10 -0400

    Add POST request

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const fs = require('fs');
 const app = express();
+
+// Middleware
+app.use(helmet());
+app.use(morgan('short'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -14,6 +22,23 @@ app.get('/api/v1/tours', (req, res) => {
       tours: tours
     }
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = { ...req.body, id: newId };
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err, data) => {
+      if (err) console.log(err);
+      res.status(201).json({
+        status: 'success',
+        data: { tour: newTour }
+      });
+    }
+  );
 });
 
 const PORT = 3000;
